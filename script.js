88c9a0bb85a2ed09b72eb1ed180c8599c47320d6
MarianaVanucci <marianavanucciandrade@gmail.com>
2021-09-08 14:20:07 -0300

    javascript done

--- script.js
+++ script.js
@@ -2,38 +2,20 @@
 
 console.log('estou funcionando mesmo?')
 
+var email = document.querySelector("#email");
 
+    email.addEventListener("focus",()=>{
+        email.style.borderColor = "cadetblue";
+});
+    email.addEventListener("blur",()=>{
+        email.style.borderColor = "#ccc";
+});
 
+var password = document.querySelector("#password");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    password.addEventListener("focus",()=>{
+        password.style.borderColor = "cadetblue";
+});
+    password.addEventListener("blur",()=>{
+        password.style.borderColor = "#ccc";
+});
